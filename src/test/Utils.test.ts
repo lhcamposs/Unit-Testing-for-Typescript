5a798feb49c72de5f432a0b3fe8d4c5d6c8fc0e8
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-02-19 11:42:11 -0300

    estrutura de um teste unitario

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,15 @@
 import { toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("Should return upppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+  it("Should return upppercase of valid string", () => {
+    // arrange:
+    const sut = toUpperCase;
+    const expected = "ABC";
+
+    //act:
+    const actual = sut("abc");
+
+    // assert:
+    expect(actual).toBe(expected);
   });
 });
