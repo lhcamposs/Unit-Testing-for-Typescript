e47de9cad13d8d1e292bd059b33cd6ddbfcecee6
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-02-20 12:18:02 -0300

    jest: afirmações e comparações

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("Should return upppercase of valid string", () => {
@@ -11,5 +11,36 @@ describe("Utils test suite", () => {
 
     // assert:
     expect(actual).toBe(expected);
+  });
+
+  it.only("Should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain("M");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).not.toBeDefined();
+    expect(actual.extraInfo).not.toBeTruthy();
   });
 });
