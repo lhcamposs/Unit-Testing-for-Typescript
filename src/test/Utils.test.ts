93c4dd8143bb6bb60b9536b4068271eb27dcba4b
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-02-20 12:41:12 -0300

    jest: testes parametrizados

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -13,6 +13,17 @@ describe("Utils test suite", () => {
     expect(actual).toBe(expected);
   });
 
+  describe("ToUpperCase exemples", () => {
+    it.each([
+      { input: "abc", expected: "ABC" },
+      { input: "My-String", expected: "MY-STRING" },
+      { input: "def", expected: "DEF" },
+    ])("$input toUpperCase should be $expected", ({ input, expected }) => {
+      const actual = toUpperCase(input);
+      expect(actual).toBe(expected);
+    });
+  });
+
   describe("getStringInfo for arg My-String should", () => {
     test("return right length", () => {
       const actual = getStringInfo("My-String");
